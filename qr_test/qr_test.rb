9f65cacb1fe6a74dcc679c2aff35a2ea789ef777
William Oberndorfer <will.oberndorfer@gmail.com>
2015-04-18 01:59:08 -0400

    reading qr codes with opencv

--- qr_test/qr_test.rb
+++ qr_test/qr_test.rb
@@ -1,9 +1,26 @@
+#!/usr/bin/env ruby
+# qr_test.rb
+
 require 'zxing'
 require 'opencv'
 
-capture = OpenCV::CvCapture.open
+include OpenCV
 
-puts ZXing.decode 'images/red.png'
-puts ZXing.decode 'images/green.png'
-puts ZXing.decode 'images/yellow.png'
-puts ZXing.decode 'images/blue.png'
+# initialize webcam
+capture = CvCapture.open
+# make a window for testing cam input
+window = GUI::Window.new("qr test")
+
+# capture and display image, decode and write result
+image = capture.query
+image.save('test.jpg')
+window.show(image)
+puts ZXing.decode 'test.jpg'
+
+# wait for a key press in image window
+GUI::wait_key
+
+# puts ZXing.decode 'images/red.png'
+# puts ZXing.decode 'images/green.png'
+# puts ZXing.decode 'images/yellow.png'
+# puts ZXing.decode 'images/blue.png'
